c89ecfe2f66ca1c4a7795b00723685271b434250
madaKaisutam <madaoikonip@gmail.com>
2023-03-24 18:35:23 +0100

    site test

---
+++ mainApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    dom = 'Hello World'
+    context = {
+        'dom' : dom,
+    }
+    return render(request, 'main.html', context)
