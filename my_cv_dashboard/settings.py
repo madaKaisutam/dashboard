68e73c4a04c5eb58cefd8a7234c4304c35f5aac5
madaKaisutam <madaoikonip@gmail.com>
2023-03-27 19:12:30 +0200

    mysql connection setup

--- my_cv_dashboard/settings.py
+++ my_cv_dashboard/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'my_cv_dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'dashboard',
+	  'USER': 'adam',
+          'PASSWORD': 'adakj#$Dh[!dcfdf3?',
+          'HOST': 'localhost',
+          'PORT': '3306',
+     }
 }
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
